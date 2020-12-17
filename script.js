9bd2f3099e9cfdcb53a31eb7c1794a366c1bcce6
sernili <li.sachse@gmail.com>
2020-12-17 16:38:18 +0100

    fix colors hamburger + hide scroll btn at top

--- script.js
+++ script.js
@@ -10,13 +10,55 @@ document.querySelector('.hamburger-menu').addEventListener('click', () =>
     document.querySelector(".container").classList.toggle("change"))
 
 
-// For soft scroll
+// For soft scroll - Up-Arrow
 document.querySelector('.scroll-btn').addEventListener("click", () => {
     document.querySelector('html').style.scrollBehavior = "smooth";
+        
     setTimeout(() => {
         document.querySelector('html').style.scrollBehavior = "unset";
-    }, 1000
-    )
+    }, 1000)
+});
 
-})
+// For soft scroll - Sidebar
+document.querySelectorAll('.menu-link').forEach(anchor => {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const hrefAttribute = document.querySelector(anchor.getAttribute('href'));
+        hrefAttribute.scrollIntoView({ behavior: 'smooth'});
+
+        document.querySelector(".container").classList.toggle("change");
+    });
+});
+
+// Color Change Hamburger in sections with white background
+const mybutton = document.querySelector(".scroll-btn");
+mybutton.classList.add('hidden');
+
+let hidden = true;
+
+window.onscroll = function(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (mybutton.classList.contains('hidden')) {
+            mybutton.classList.remove('hidden');
+            console.log("REMOVE")
+        } 
+        
+    } else {
+        
+
+        if (!mybutton.classList.contains('hidden')) {
+            mybutton.classList.add('hidden');
+            console.log("ADD")
+        } 
+    }
+}
+        
+// document.addEventListener("scroll", () => {
+//     let start = document.querySelector("#about-us");
+//     let end = document.querySelector(".container");
+//     if(start.getBoundingClientRect().top == 0) {
+//       console.log('Here!');
+//     }
+// })
 
