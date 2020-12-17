a82dd5fc7a48166a547c33e5de2e8a95fea275e8
sernili <li.sachse@gmail.com>
2020-12-17 13:58:04 +0100

    add spinner animation on load

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Load Container after spinner
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display")
+    }, 2000)
+}
+
 // Add Class "change" to CONTAINER when hamburger menu is clicked
 document.querySelector('.hamburger-menu').addEventListener('click', () => 
     document.querySelector(".container").classList.toggle("change"))
@@ -12,3 +19,4 @@ document.querySelector('.scroll-btn').addEventListener("click", () => {
     )
 
 })
+
